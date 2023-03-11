6d7a73f1121bb25f9e0bb1c4dd51c7fdac357568
Hezakai <graves.eric@gmail.com>
2023-03-11 14:48:58 -0600

    updated server.js

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -24,8 +24,15 @@ const server = new ApolloServer({
   resolvers,
 });
 
-server.applyMiddleware({ app });
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start()
+  server.applyMiddleware({ app })
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`)
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+    })
+  })
+}
+startApolloServer(typeDefs, resolvers);
